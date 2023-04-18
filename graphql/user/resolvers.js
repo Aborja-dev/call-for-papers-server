c7f4cf7832c4c680f0ce4872086fcdbbf96a068f
ankahara <hecatombe100@gmail.com>
2023-04-17 21:58:32 -0600

    hashear las contraseñas

--- graphql/user/resolvers.js
+++ graphql/user/resolvers.js
@@ -1,10 +1,12 @@
-import { users } from "../../mocks/users.js"
+import { usersWithHash, users } from "../../mocks/users.js"
 
-const getUser = (_, {username}) => users[0]
+const getUser = (_, {username}) => usersWithHash[0]
 
-const getUsers = () => users
+const getUsers = () => {
+    return usersWithHash
+}
 
-const createUser = (_, {username, password, email, name}) => users[0]
+const createUser = (_, {username, password, email, name}) => usersWithHash[0]
 
 const deleteUser = (_, {userID}) => 'User deleted'
 
